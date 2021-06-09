2aaa4e5d8e1b85af43d0bef09f30e61f7af7aa29
Cola0165 <zwq1773677992@sina.com>
2021-06-09 09:32:43 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 创建/删除指定目录：xxxxx
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = "";
+                string path = @"C:\CrtDire123";
+                try
+                {
+                    if (Directory.Exists(path))
+                    {
+                        Directory.Delete(path, true);
+                        textBox1.Text = "删除文件夹成功";
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(path);
+                        textBox1.Text = "创建文件夹成功";
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text = ex.Message;
+                    //throw;
+                }
+                finally
+                {
+                    textBox1.Text += Environment.NewLine + "操作已完成！";
+                }
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = @"c:\0608\FileTest.txt";
+                //string path2 = @"c:\temp\NewFileTest.txt";
+                if (!File.Exists(path))
+                {
+                    //Directory.CreateDirectory(@"c:\a");
+                    // 创建一个文件用于写入UTF-8编码的文本
+                    StreamWriter sw = File.CreateText(path);
+                    sw.WriteLine("You");
+                    sw.WriteLine("are");
+                    sw.WriteLine("Beautiful");
+                    //sw.Dispose();
+                    sw.Close();
+                }
+                // 打开文件，从里面读出数据
+                StreamReader sr = File.OpenText(path);
+                string s = "";
+                //输出文件里的内容，直到文件结束
+                while ((s = sr.ReadLine()) != null)
+                {
+                    textBox1.Text += s+Environment.NewLine;
+                }
+                //sr.Dispose();
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream myFileStream = new FileStream("c:\\BinFile.dat", FileMode.OpenOrCreate);
+                FileStream fileStream = new FileStream("c:\\BinFile1.dat", FileMode.OpenOrCreate);
+                // 为文件流创建二进制写入器
+                BinaryWriter myBinaryWriter = new BinaryWriter(myFileStream);
+                // 写入数据
+                for (int i = 65; i < 71; i++)
+                {
+                    myBinaryWriter.Write((char)i);
+                }
+                myBinaryWriter.Close();
+                myFileStream.Close();
+
+                // 创建reader
+                myFileStream = new FileStream("c:\\BinFile.dat", FileMode.Open, FileAccess.Read);
+                BinaryReader myBinaryReader = new BinaryReader(myFileStream);
+                // 从BinFile读数据
+                while (myBinaryReader.PeekChar() != -1)
+                {
+                    textBox1.Text += myBinaryReader.ReadChar().ToString();
+                }
+                myBinaryReader.Close();
+                myFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ c12_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace c12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //指定目录的路径
+            string path = @"C:\CrtDire123";
+            try
+            {
+                //判断目录是否存在
+                if (!Directory.Exists(path))
+                {
+                    //如果不存在则创建目录
+                    Directory.CreateDirectory(path);
+                    Console.WriteLine("创建目录成功");
+                }
+                else
+                {
+                    //如果目录存在，则删除该目录
+                    Directory.Delete(path,true );
+                    //Directory.Delete(path);
+                    Console.WriteLine("删除目录成功");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("处理过程失败：{0}", e.ToString());
+            }
+            finally { }
+            Console.Read();
+        }
+    }
+}
